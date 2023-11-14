bfd1c22dec4b0387aafed24ecae0b41930555fbe
mathewkoech <mathewkoech55@gmail.com>
2023-11-14 06:46:57 -0800

    modifiers

---
+++ flags.c
@@ -0,0 +1,32 @@
+include "main.h"
+
+/**
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ */
+int get_flags(const char *format, int *i)
+{
+	int j, start_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_AR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (start_i = *i + 1; format[start_i] != '\0'; start_i++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+		{
+			if (format[start_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_AR[j];
+				break;
+			}
+		}
+		if (FLAGS_CH[j] == 0)
+			break;
+	}
+	*i = start_i - 1;
+
+	return (flags);
+}
---
+++ size.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *get_length- calculates the size of the argument
+ *@format:string arguments
+ *@i:arguments
+ *Return: size
+ */
+
+int get_length(const char *format, int *i)
+{
+	int size = 0;
+	int start_i = *i + 1;
+
+	if (format[start_i == 'l'];)
+	{
+		size = S_LONG;
+	}
+	if (format[start_i == 'h'])
+	{
+		size = S_SHORT;
+	}
+
+	if (size == 0)
+	{
+		*i = start_i - 1;
+	}
+
+	else
+		*i = start_i;
+
+	return (size);
+}
